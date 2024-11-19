89abe82ee177df54ccf5c0a8f81c414964a819e8
lemisora <leonardo.michel.dom@gmail.com>
2024-11-19 13:20:51 -0600

    Se documenta el header de utilidades y el programa principal

--- src/my_shell.c
+++ src/my_shell.c
@@ -1,23 +1,26 @@
 #include "utilities.h"
-#include <locale.h>
-#include <pwd.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <unistd.h>
+#include <locale.h> //Header para poder mostrar los mensajes de perror() en el idioma del sistema
+#include <pwd.h>    //Header para acceder a propiedades del usuario actual para el prompt
+#include <stdbool.h>    //Header que provee a C la capacidad de usar booleanos
+#include <stdio.h>      //Funciones de entrada/salida
+#include <stdlib.h>     //Otorga la función exit() y los macros para salida exitosa y salida con error
+#include <string.h>     //Otorga funciones para manejo de cadenas
+#include <unistd.h>     //Otorga funciones para imprimir información útil en el prompt
 
-#define KW_NUM 11
+#define KW_NUM 11   //Número de palabras reservadas (comandos) que interpreta la shell
 
-const char *user;
-char hostname[BUF_LENGTH];
+const char *user;   //Aquí se almacena el nombre del usuario actual
+char hostname[BUF_LENGTH];  //Aquí se almacena el hostname del usuario actual
 
+//Arreglo de comandos disponibles
 char *keywords[KW_NUM] = {"exit", "help",  "touch", "write", "rm",   "cat",
                           "mv",   "mkdir", "ls",    "rmdir", "clear"};
 
+//Imprimir el prompt
 void printPrompt(const char *usr, char *hostname) {
-  char dir_actual[PATH_MAX_LENGTH];
+  char dir_actual[PATH_MAX_LENGTH]; //Obtener la ruta actual
+
+  //Se imprime el prompt en color verde
   if (getcwd(dir_actual, sizeof(dir_actual)) != NULL) {
     printf("\033[1m\033[32m[%s@%s\033[0m\033[1m:%s]$ \033[0m", user,
            hostname, dir_actual);
@@ -27,22 +30,27 @@ void printPrompt(const char *usr, char *hostname) {
   }
 }
 
+//Imprimir mensaje inicial
 void printInitMsg() {
   printf("Bienvenido a My Own Shell (Development)!\nPara más información "
          "acerca de los comandos disponibles ingrese el comando: 'help'\n");
 }
 
+//Imprimir mensaje de salida
 void printExitMsg() { printf("Saliendo de mosh-dev\n"); }
 
+//Función para limpiar la pantalla
 void clear_screen() { printf("\033[2J\033[H"); }
 
+//Función para mapear comandos
 int shellKeyword(char *command) {
-  for (int i = 0; i < KW_NUM; i++)
-    if (strcmp(keywords[i], command) == 0)
-      return i;
-  return -1;
+    for (int i = 0; i < KW_NUM; i++)
+        if (strcmp(keywords[i], command) == 0)
+            return i;
+    return -1;
 }
 
+//Función para mostrar más información sobre los comandos disponibles en mosh
 void showHelp() {
   printf("Los comandos disponibles en esta shell son los siguientes:\n");
   printf("touch - Crear un archivo vacío\n\tUso: touch [nombre_archivo]\n");
@@ -70,11 +78,12 @@ void showHelp() {
   printf("exit - Para salir de esta shell\n");
 }
 
-int main() {
-  int argc;
-  if (setlocale(LC_ALL, "") == NULL)
+int main(void) {
+  int argc; //Variable para contar los argumentos de cada una de las utilidades
+  if (setlocale(LC_ALL, "") == NULL)    //Se configura el idioma para mostrar mensajes de error del sistema con perror()
     perror("Error al configurar localización de idioma");
 
+  //Obtener UID del usuario
   uid_t uid = getuid();
   struct passwd *pw = getpwuid(uid);
 
@@ -83,71 +92,77 @@ int main() {
     return EXIT_FAILURE;
   }
 
+  //Obtener el nombre del usuario y el hostname
   user = pw->pw_name;
   if (gethostname(hostname, sizeof(hostname)) == -1) {
     perror("Error al iniciar shell, no se encuentra hostname");
     return EXIT_FAILURE;
   }
 
+  //Se reserva memoria para el buffer de caracteres
   char buffer[BUF_LENGTH];
-  printInitMsg();
+  printInitMsg(); //Mensaje inicial
 
   while (1) {
-    printPrompt(user, hostname);
+    printPrompt(user, hostname);    //Se imprime el prompt
+
+    //Interpretación de comandos ingresados
     if (fgets(buffer, BUF_LENGTH, stdin) != NULL) {
-      buffer[strcspn(buffer, "\n")] = '\0';
+      buffer[strcspn(buffer, "\n")] = '\0'; //Reformateo de la cadena para hacerla válida
 
-      if(strlen(buffer) == 0){
-        continue;
-      }
+      if(strlen(buffer) == 0) continue; //Pasar al siguiente ciclo de reconocimiento de comando si se ingresa un ENTER
 
+      //Tokenización de argumentos
       char *args[BUF_LENGTH / 2 + 1];
       char *token = strtok(buffer, " ");
-      argc = 0;
+      argc = 0; //Reinicialización del contador de argumentos
       while (token != NULL && argc < BUF_LENGTH) {
         args[argc++] = token;
         token = strtok(NULL, " ");
       }
       args[argc] = NULL;
 
+      //Mapeo de comandos
       int cmd_mapper = shellKeyword(args[0]);
+
+      //Verificación de validez de comando y ejecución de la tarea que le corresponde
       switch (cmd_mapper) {
-      case 0:
+      case 0:   //Salir de a shell
         printExitMsg();
         exit(EXIT_SUCCESS);
         break;
-      case 1:
+      case 1:   //Imprimir ayuda
         showHelp();
         break;
-      case 2:
+      case 2:   //Comando touch
         my_touch(argc, args);
         break;
-      case 3:
+      case 3:   //Comando write
         my_cat_append(argc, args);
         break;
-      case 4:
+      case 4:   //Comando rm
         my_rm(argc, args);
         break;
-      case 5:
+      case 5:   //Comando cat
         my_cat(argc, args);
         break;
-      case 6:
+      case 6:   //Comando mv
         my_mv(argc, args);
         break;
-      case 7:
+      case 7:   //Comando mkdir
         my_mkdir(argc, args);
         break;
-      case 8:
+      case 8:   //Comando ls
         my_ls(argc, args);
         break;
-      case 9:
+      case 9:   //Comando rmdir
         my_rmdir(argc, args);
         break;
-      case 10:
+      case 10:  //Comando clear
         clear_screen();
         break;
-      default:
-        printf("\033[1m\033[31mError:\033[0m Comando '%s' no reconocido\n",
+      default:  //Comando desconocido
+        printf("\033[1m\033[31mError:\033[0m Comando '%s' no reconocido\n", //Se imprime un error resaltado en rojo
                buffer);
         break;
       }
--- src/utilities.h
+++ src/utilities.h
@@ -1,18 +1,40 @@
+/*
+* Headers que proveen acceso a las funciones prototipadas
+* Las funciones aquí descritas son las utilidades que usa mosh para interactuar con los archivos y directorios del sistema de archivos
+* mosh-utils
+*/
+
 #ifndef MOSH_UTILS_H
 #define MOSH_UTILS_H
 
+//Tamaño determinado para los buffers
 #define BUF_LENGTH 1024
+
+//Tamaño determinado para las rutas
 #define PATH_MAX_LENGTH 1024
 
-#include <stdio.h>
-
+//Función para crear archivos vacíos
 int my_touch(int argc, char **args);
+
+//Función para escribir texto a un archivo, se llama en la shell con el comando 'write'
 int my_cat_append(int argc, char **args);
+
+//Función para eliminar archivos, tiene dentro una versión recursiva para eliminar un directorio que contenga archivos
 int my_rm(int argc, char **args);
+
+//Función para leer e imprimir el texto de un archivo a la salida estándar
 int my_cat(int argc, char **args);
+
+//Función para mover o renombrar archivos y directorios
 int my_mv(int argc, char **args);
+
+//Función para crear directorios
 int my_mkdir(int argc, char **args);
+
+//Función para listar los archivos y directorios contenidos dentro del directorio actual o de la ruta especificada
 int my_ls(int argc, char **args);
+
+//Función para eliminar directorios vacíos
 int my_rmdir(int argc, char **args);
 
 #endif
